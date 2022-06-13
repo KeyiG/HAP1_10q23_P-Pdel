a20723015bc08d98ca5244e71c692506c0004954
KeyiG <34516540+KeyiG@users.noreply.github.com>
2022-06-13 16:09:40 +0200

    Update align_PE.sh

--- RNA-seq/PE/align_PE.sh
+++ RNA-seq/PE/align_PE.sh
@@ -20,7 +20,6 @@ FQ_PATH=/proj/rmrRNA/PE
 OUTPUT_PATH=/proj/alignment/PE
 REF_PATH=/proj/reference
 
-#loop to run HiSAT2 alignment to rRNA, and keeping unaligned reads.
 
 cd ${FQ_PATH}
 for i in $(ls *_tc_rmrRNA.fastq.1.gz | sed 's/_tc_rmrRNA.fastq.1.gz//g'); do	
@@ -41,12 +40,10 @@ done
 #Readme:
 # -k Default: 5 (linear index) or 10 (graph index).
 #-p specifies the number of computational cores/threads that will be used by the program
-#--rna-strandness: strand-specific information. Needs to be RF if using Illumina Truseq library preparation. R or RF is the choice for dUTP based method which is used by most of the stranded lib prep kit. R for SE and RF for PE.
+#--rna-strandness: strand-specific information. R or RF is the choice for dUTP based method which is used by most of the stranded lib prep kit. R for SE and RF for PE.
 #--un-conc-gz: Write paired-end reads that fail to align concordantly to file(s) at <path>. Useful for rRNA removal
 #--summary-file: Print alignment summary to this file
 #-x path to the pre-built genome index. Note that the index consists of multiple files ending in .ht2 , and only the shared part of the filename should be indicated (e.g. genome if the files are called genome.1.ht2 , genome.2.ht2 , etc).
-#--dta: this option is present in JOnas script. it is for With this option, HISAT2 requires longer anchor lengths for de novo discovery of splice sites. This leads to fewer alignments with short-anchors, which helps transcript assemblers improve significantly in computation and memory usage. for transcript assembly. I don't think it is very necessary for our purpose. so I skip it.
-#-1 the first-read mate FASTQ file
 #-2 the second-read mate FASTQ file
 #-S name of the result file that will be created
 #>> send all messages from HISAT2 (including errors and warnings) into the specified file
